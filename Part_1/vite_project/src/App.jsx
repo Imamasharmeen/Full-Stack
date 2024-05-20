ac4493df37b27e95194f19df8df643812a318908
Sharmeen Akter Imama <100438343+Imamasharmeen@users.noreply.github.com>
2024-05-20 19:22:49 +0300

    Add files via upload

---
+++ Part_1/vite_project/src/App.jsx
@@ -0,0 +1,29 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
